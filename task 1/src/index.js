709c1bcfff19a72d1139df53178e3c4a29db08a9
Eduard <edikzaxarin@gmail.com>
2019-03-01 20:24:17 +0200

    added jquery

--- task 1/src/index.js
+++ task 1/src/index.js
@@ -1,39 +1,34 @@
-window.addEventListener("load",handler,false);
-
+$(document).ready(handler);
 function handler(e){
-    document.forms.searchForm.addEventListener("submit",handlerSearch,false);
-    handlerLinksLoad()
-    document.getElementById("jsLinks").addEventListener("click",handlerLinks,false);
+    $("form[name='searchForm']").on({submit:handlerSearch});
+    $("#jsLinks").on("click","li a",handlerLinks);
+    $("#jsLinks").on("click","h3",handlerH);
+    handlerLinksLoad();    
+}
+function handlerH(){
+    clearLinks();
 }
 function handlerSearch(e){
     alert("hello");
     return e.preventDefault();
 }
 function handlerLinksLoad(){
-    let ul = document.getElementById("jsLinks").children[1];
+    let ul = $("#jsLinks ul");
+    let frag = document.createDocumentFragment();
     for (let index = 0; index < 4; index++) {
-        let li = document.createElement("li");
-        li.setAttribute("class","nav-item");
-        let a = document.createElement("a");
-        a.setAttribute("class","nav-link");
-        a.setAttribute("href","#");
-        a.innerHTML = "Link"
-        li.appendChild(a);
-        ul.appendChild(li);
+        $(frag).append("<li class='nav-item'><a class='nav-link' href='#'>Link</a></li>")
     }
+    ul.append(frag);
+}
+function clearLinks(){
+    let ul = $("#jsLinks ul");
+    let list = ul.find("a");
+    list.removeClass("active");
+    list.text("Link");
 }
 function handlerLinks(e){
-    if(e.target.tagName!="A"&&e.target.tagName!="H3")
-        return;
-    for (let index = 0; index < e.currentTarget.children[1].children.length; index++) {
-        const element = e.currentTarget.children[1].children[index];
-        let el = element.children[0];
-        el.innerHTML = "Link";
-        el.setAttribute("class","nav-link");
-    }
-    if(e.target.tagName=="A"){
-        e.target.setAttribute("class","nav-link active");
-        e.target.innerHTML = "Active";        
-    }
+    clearLinks();
+    $(this).text("Active");
+    $(this).addClass("active");
     return e.preventDefault();
 }
